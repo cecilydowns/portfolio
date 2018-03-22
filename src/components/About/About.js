56042a513e384fdb8f3ddb5215a239d852bda4a5
Cecily <cecily.downs@gmail.com>
2018-03-21 18:43:36 -0700

    Initial About page setup

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+
+  render() {
+    return (
+      <div id="about-container">
+        <p>Read All About Me</p>
+        <p>Should mention here what kind of technologies I know</p>
+      </div>
+    );
+  }
+}
+
+export default About;
