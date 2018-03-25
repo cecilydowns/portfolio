9158c3af7162a1d772700d224c64f76c5ca0644d
Cecily <cecily.downs@gmail.com>
2018-03-25 13:06:05 -0700

    Incorporate CSS modules into navbar components

--- src/components/Navbar/NavItem.js
+++ src/components/Navbar/NavItem.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import styles from './Navbar.scss';
+
 
 const NavItem = (props) => {
     return (
             <NavLink
             exact
             to={props.to}
-            activeClassName="selected"
+            activeClassName={styles.selected}
             >
-               <div className="navitem">
-                    <div className="navimage">
+               <div className={styles.navitem}>
+                    <div className={styles.navimage}>
                         <img src={props.icon} />
                     </div>
-                    <div className="navtext">
+                    <div className={styles.navtext}>
                         {props.name}
                     </div>
               </div>
