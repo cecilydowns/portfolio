07f15dc3bb02bb103889625b094f8087fcf06d60
Cecily <cecily.downs@gmail.com>
2018-03-28 16:03:58 -0700

    Add Post component for blog post items

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import styles from './Home.scss'
 
 const Post = (props) => {
     const tags = props.tags.map((tag, key) => {
-        return <div key={key} className={`tag tag-${tag.slug}`}>{tag.name}</div>
+        return <div key={key} className={styles.tag}>
+            <div className={styles.dot}></div>
+            {tag.name}
+        </div>
     })
 
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+    ];
+
+    const date = new Date(props.date)
+    const formattedDate = `${monthNames[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+
     return (
-            <div className="post">
-                <a href={`http://cecilycodes.com${props.url}`}>
-                    {props.title}
-                </a>
-                <div>{tags}</div>
+            <div className={styles.post}>
+                <div>
+                    <a href={`http://cecilycodes.com${props.url}`}>
+                        {props.title}
+                    </a>
+                </div>
+
+                <div className={styles.postInfo}>
+                    <strong>{formattedDate}</strong> | {tags}
+                </div>
+
             </div>
             );
 }
