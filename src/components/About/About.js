8b1ea367c3179d4ea2568dd39a68c64c3a896d88
Cecily <cecily.downs@gmail.com>
2018-03-31 14:59:39 -0700

    Update About page content and styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import './About.css';
+import styles from './About.scss';
+import profile from './cecily.jpg';
+import resume from './resume.png';
 
 class About extends Component {
 
   render() {
     return (
-      <div id="about-container">
-        <p>Read All About Me</p>
-        <p>Should mention here what kind of technologies I know</p>
+      <div className="content-container">
+        <div className={styles.flexContainer}>
+
+          <div className={styles.right}>
+              <h2 className={styles.heading}>I’m a full stack web developer with a passion for creating beautiful and user-friendly web applications.</h2>
+              
+              <p className={styles.subhead}>Merging a decade of experience building web projects with an education in storytelling and communications, I excel at adapting to new technologies and finding creative solutions that help companies engage their users throughout the digital journey.</p>
+
+          </div>
+
+          <div className={styles.left}>
+              <img src={profile} alt="Cecily" className={styles.profile} />
+          </div>
+
+        </div>
+
+        <div className={styles.flexContainerBottom}>
+            <div className={styles.skillsContainer}>
+              <h3 className={styles.cursive}>Technical Skills</h3>
+              <ul className={styles.skills}>
+                <li><span className={styles.dot}></span>HTML5</li>
+                <li><span className={styles.dot}></span>CSS3</li>
+                <li><span className={styles.dot}></span>SASS</li>
+                <li><span className={styles.dot}></span>JavaScript/ES6</li>
+                <li><span className={styles.dot}></span>React</li>
+                <li><span className={styles.dot}></span>Redux</li>
+                <li><span className={styles.dot}></span>AJAX</li>
+                <li><span className={styles.dot}></span>APIs</li>
+                <li><span className={styles.dot}></span>Ruby on Rails</li>
+              </ul>
+            </div>
+
+             <div className={styles.resumeContainer}>
+              <h3 className={styles.cursive}>Resume</h3>
+              <img src={resume} alt="Resume" className={styles.resume} />
+            </div>
+
+          </div>
       </div>
     );
   }
