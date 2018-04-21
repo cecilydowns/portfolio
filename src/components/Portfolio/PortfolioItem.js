f9d66d3daf8a241108bb548f9f986e3f7409d6e4
Cecily <cecily.downs@gmail.com>
2018-04-21 16:19:41 -0700

    Adjust portfolio show page

--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
+import styles from './Portfolio.scss';
 
-const PortfolioItem = (props) => {
+class PortfolioItem extends Component {
 
-    const tags = props.tags.map(tag => {
-        return <div key={tag.id} className="tag"><span className="dot"></span>{tag.name}</div>
-    })
+    constructor(props){
+        super(props)
+    }
 
-    return (
-        <div className="portfolio-item" style={{ backgroundImage: `url(${props.img})` }}>
-            <div className="portfolio-item-label">
-                <h3>{props.name}</h3>
-                <div className="portfolio-item-description">
-                    {props.description}
-                    <div className="buttons">
-                        <a href="/" className="button">Read More</a>
-                        <a href="/" className="button">Live Demo</a>
-                        <a href="/" className="button">Walkthrough</a>
-                        <a href="/" className="button">Github</a>
+    render(){
+
+        const tags = this.props.tags.map(tag => {
+            return <div key={tag.id} className={styles.tag}><span className={styles.dot}></span>{tag.name}</div>
+        })
+
+        return (
+            <div className={styles.portfolioItem} style={{ backgroundImage: `url(${this.props.img})` }}>
+                <div className={styles.portfolioItemLabel}>
+                    <h3>{this.props.name}</h3>
+                    <div className={styles.portfolioItemDescription}>
+                        {this.props.description}
+                        <div className={styles.buttons}>
+                            <a href="/" className={styles.button}>Read More</a>
+                            <a href="/" className={styles.button}>Live Demo</a>
+                            <a href="/" className={styles.button}>Walkthrough</a>
+                            <a href="/" className={styles.button}>Github</a>
+                        </div>
                     </div>
+    
                 </div>
-
+    
+                <div className={styles.portfolioItemTags}>
+                    {tags}                    
+                </div>
             </div>
+        );
+    }
 
-            <div className="portfolio-item-tags">
-                {tags}                    
-            </div>
-        </div>
-    );
 }
 
 export default PortfolioItem;
